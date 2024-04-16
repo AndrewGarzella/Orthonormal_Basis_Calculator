e2715fada97537e8952781fb861036017fde7165
AndrewGarzella <andrew.garzella@gmail.com>
2024-04-15 19:30:10 -0600

    Calculates Orthogonal Basis for a given matrix

---
+++ ONB_calc.jl
@@ -0,0 +1,21 @@
+using LinearAlgebra
+
+function Create_ONB(Mtrx)
+    Basis = Vector{Float64}[]
+    
+    for i in 1:size(Mtrx)[2]
+        push!(Basis, Mtrx[:, i])
+    end
+
+    for i in 1:size(Basis)[1]
+        for j in 1:i-1
+            Basis[i] = Basis[i] - (dot(Basis[j], Mtrx[:, i])/dot(Basis[j], Basis[j]))Basis[j]
+        end
+        Basis[i] = (1/sqrt(dot(Basis[i], Basis[i])))Basis[i]
+        println(dot(Basis[i], Basis[i]))
+    end
+    
+    return Basis
+   
+end
+
